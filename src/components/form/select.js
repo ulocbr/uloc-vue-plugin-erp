7e882f2b4bb744fb69edcef3c4784830c605f41d
Tiago Felipe <tiago@tiagofelipe.com>
2020-03-01 11:42:18 -0300

    Loading in ErpSelect

--- src/components/form/select.js
+++ src/components/form/select.js
@@ -400,8 +400,8 @@ export default {
           ],
         (this.isLoading && h(USpinner, {
           slot: 'after',
-          staticClass: 'u-if-control',
-          props: {size: '24px'}
+          staticClass: 'erp-if-control',
+          props: {size: '11px'}
         })) || void 0
       ].concat([h('div', {staticClass: 'erp-select-line-helper'}), this.$slots.default]))
   }
