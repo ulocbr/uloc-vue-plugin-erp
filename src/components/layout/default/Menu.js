d796ee35ff3bd009e261c1bb34ab5aad5009a53a
Tiago Felipe <tiago@tiagofelipe.com>
2020-03-21 21:18:32 -0300

    Show menu on click while is folded

--- src/components/layout/default/Menu.js
+++ src/components/layout/default/Menu.js
@@ -35,19 +35,24 @@ export default {
   },
   beforeDestroy () {
   },
-  watch: {
-  },
+  watch: {},
   methods: {
     setActiveMenu (id) {
       this.activeMenu = id
       this.$emit('setActiveMenu', id)
+    },
+    click () {
+      this.erpheader && this.erpheader.modulemenu && this.erpheader.modulemenu.toggledMenuModule && this.erpheader.modulemenu.__toggleMenuModule(false)
     }
   },
   render (h) {
     return h('div', {
       staticClass: 'u-erp-menu',
       'class': this.computedClass,
-      style: this.computedStyle
+      style: this.computedStyle,
+      on: {
+        click: this.click
+      }
     }, [
       this.$slots.default
     ])
